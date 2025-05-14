ac5122ddd9b99c52e7aae98431d6294d0026338b
tarbi <tarbipyakurel21@gmail.com>
2025-05-14 10:14:40 -0500

    Added NprToUsd to convert user entered Nepalese currency to USD

---
+++ Basics/NprToUsd.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class NprToUsd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter amount to convert: ");
+        double amount = sc.nextDouble();
+        double usd = amount / 136.5;
+        System.out.printf("USD: %.2f", usd);
+    }
+}
