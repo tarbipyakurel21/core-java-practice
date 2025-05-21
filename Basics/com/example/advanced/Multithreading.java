97904e83d36de1ddc3f0ff9026357c8cd95c7845
tarbipyakurel21 <tarbipyakurel21@gmail.com>
2025-05-20 20:38:22 -0500

    Update Multithreading.java

--- Basics/com/example/advanced/Multithreading.java
+++ Basics/com/example/advanced/Multithreading.java
@@ -1,5 +1,32 @@
 package com.example.advanced;
+class MyWorker implements Runnable {
+    private String name;
+    public MyWorker(String name) { this.name = name; }
+
+    @Override
+    public void run() {
+        System.out.println(name + " started.");
+        try {
+            Thread.sleep(50); // Simulate some work
+        } catch (InterruptedException e) {
+            System.out.println(name + " interrupted.");
+        }
+        System.out.println(name + " finished.");
+    }
+}
 
 public class Multithreading {
+    public static void main(String[] args) throws InterruptedException {
+        Thread worker1 = new Thread(new MyWorker("Worker-1"));
+        Thread worker2 = new Thread(new MyWorker("Worker-2"));
 
+        worker1.start(); // Start Worker-1
+        worker2.start(); // Start Worker-2
+
+        System.out.println("Main thread running.");
+
+        worker1.join(); // Wait for Worker-1 to complete
+        worker2.join(); // Wait for Worker-2 to complete
+        System.out.println("All workers done.");
+    }
 }
